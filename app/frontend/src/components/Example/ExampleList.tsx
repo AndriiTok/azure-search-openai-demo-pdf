c1089de04e411cd389a13c77bff66330e6cc0cca
Andrii Tokarskyi <>
2023-06-12 07:15:37 +0300

    Clean up UI

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,13 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What can I ask?",
+        value: "What can I ask?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { 
+        text: "How can you help me?", 
+        value: "How can you help me?"
+    }
 ];
 
 interface Props {
